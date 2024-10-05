ce1d1b1519ce77f9a9c2a978fe7961c7d37df726
10prettyminion01 <sacredminion1001@gamil.com>
2024-10-05 05:56:07 -0400

    update homepage with new feature

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,13 +7,32 @@ const Product = ({product}) => {
   console.log(product);
   // destructure product
   const {id, images, name, title, price} = product;
-  return <div>
-    <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
-    <div className='w-full h-full flex justify-center items-center'>{}
-    <div className='w-[200px] mx-auto flex justify-center items-center '><img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={images[0]} alt=''/></div>
+  return (
+    <div>
+      <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
+        <div className='w-full h-full flex justify-center items-center'>
+          {/* Image */}
+          <div className='w-[200px] mx-auto flex justify-center items-center'>
+            <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={images[0]} alt=''/>
+          </div>
+        </div>
+              {/* button */}
+        <div className='absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center 
+        gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
+          <button>
+            <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
+              <BsPlus className='text-3xl' />
+            </div>
+          </button>
+          <Link to={'/product/${id}'} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'><BsEyeFill /></Link>
+        </div>
+      </div>
+      {/* category & title & price */}
+      <div>
+          2
+      </div>
     </div>
-    </div>
-  </div>;
+  );
 };
 
 export default Product;
